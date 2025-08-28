357540bc22593aaf8d761dd462ad245db50326b8
yuri1odayo-gif <yuri1odayo@gmail.com>
2025-08-28 22:16:14 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import math
+
+st.title("ぷにぷにスコアタ")
+st.title("スコア計算ツール")
+
+# ========= 単位変換関数 =========
+def format_number_jp(n):
+    if n >= 1e12:
+        return f"{n/1e12:.4g}兆"
+    elif n >= 1e8:
+        return f"{n/1e8:.4g}億"
+    elif n >= 1e4:
+        return f"{n/1e4:.4g}万"
+    else:
+        return f"{n:.0f}"
+
+# ========= 入力 =========
+y = st.number_input("yマネー（整数のみ）", min_value=0, value=13000, step=1, format="%d")
+
+# 整数チェック（念のため）
+if y != int(y):
+    st.error("⚠️ y は整数を入力してください")
+else:
+    y = int(y)
+
+    # ========= 計算 =========
+    x = 10 ** ((y / 0.0011392) ** (1 / 6.497))
+
+    # ========= 結果表示 =========
+    st.write(f"約 {format_number_jp(x)} ", fontsize=20)
