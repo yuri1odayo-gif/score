ba833cfa99e1affa3a6e2e786fd4200675d518ee
yuriro <yuri1odayo@gmail.com>
2025-08-29 20:49:41 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -20,15 +20,16 @@ def format_number_jp(x):
 # ========= 入力 =========
 y = st.number_input("yマネー（整数のみ）", min_value=0, value=0, step=1, format="%d")
 
-# セッションに履歴を用意
-if "history" not in st.session_state:
-    st.session_state.history = []
 
 # 整数チェック（念のため）
 if y != int(y):
     st.error("⚠️ y は整数を入力してください")
 else:
     y = int(y)
+
+# セッションに履歴を用意
+if "history" not in st.session_state:
+    st.session_state.history = []
 
     # ========= 計算 =========
     x = 10 ** ((y / 0.0011392) ** (1 / 6.497))
