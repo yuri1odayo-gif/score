df290a9d8059c5c18dc52fe9456e4888182e5f07
yuriro <yuri1odayo@gmail.com>
2025-08-29 11:21:44 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ st.write("ぷにぷにスコアタ")
 st.write("スコア計算ツール")
 
 # ========= 単位変換関数 =========
-def format_number(n):
+def format_number_jp(x):
     if n >= 10**12:
-        return f"{n/10**12:.3f}兆"
+        return f"{x/10**12:.3f}兆"
     elif n >= 10**8:
-        return f"{n/10**8:.3f}億"
+        return f"{x/10**8:.3f}億"
     else:
-        return f"{n:,.0f}"  # それ以下はカンマ区切りの整数
+        return f"{x:,.0f}"  # それ以下はカンマ区切りの整数
 
 # ========= 入力 =========
 y = st.number_input("yマネー（整数のみ）", min_value=0, value=13000, step=1, format="%d")
